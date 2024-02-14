ce849f1665dadb7542942ae77c604cafc58ed9d1
Rania-Jaberi <raniajaberi31@gmail.com>
2024-02-14 11:08:39 +0100

    Convertir Question en classe de donnees

--- app/src/main/java/com/example/classeenumeration/Questions.kt
+++ app/src/main/java/com/example/classeenumeration/Questions.kt
@@ -1,9 +1,20 @@
 package com.example.classeenumeration
 
 //Dans la classe Question, modifiez le type de données de la propriété difficulty en remplaçant String par Difficulty.
-class Question<T>(
+//Transformez Question en une classe de données en utilisant le mot clé data.
+data class Question<T>(
     val questionText: String,
     val answer: T,
     val difficulty: Difficulty
 )
+
+//Lorsqu'une classe est définie comme une classe de données, les méthodes suivantes sont implémentées :
+//
+//equals()
+//hashCode() : vous rencontrerez cette méthode lorsque vous utiliserez certains types de collections.
+//toString()
+//componentN() : component1(), component2(), etc.
+//copy()
+
+//Une classe de données ne peut pas non plus être abstract, open, sealed ou inner.
 
